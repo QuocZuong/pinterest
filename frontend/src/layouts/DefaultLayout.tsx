969f9fa47da50f6599d494069c87c7ce4bced0aa
Zuong <quocvuongle.ct@gmail.com>
2023-12-22 22:15:19 +0700

    add default layout

---
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+import Header from "../components/Header";
+
+const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+    return (
+        <>
+            <Header></Header>
+            <div className="main">{children}</div>
+        </>
+    );
+};
+
+export default DefaultLayout;
