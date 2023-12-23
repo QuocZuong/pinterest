710e5928016d55ac463be2ecaae6684a3e7dffb7
Zuong <quocvuongle.ct@gmail.com>
2023-12-23 14:39:40 +0700

    add custom colors and class for tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        colors: {
+            black: {
+                950: "#111111",
+            },
+            grey: {
+                // hover
+                100: "#e2e2e2",
+                150: "#dadada",
+                200: "#767676",
+                // active
+                300: "#828282",
+                // hover
+                400: "#878787",
+                950: "#5f5f5f",
+            },
+            transparent: "transparent",
+            purple: "#3f3cbb",
+            midnight: "#121063",
+            metal: "#565584",
+            tahiti: "#3ab7bf",
+            silver: "#ecebff",
+            "bubble-gum": "#ff77e9",
+            bermuda: "#78dcca",
+        },
+        extend: {
+            textColor: {
+                white: "#ffffff",
+                dark: "#000000",
+            },
+        },
     },
     plugins: [],
 };
