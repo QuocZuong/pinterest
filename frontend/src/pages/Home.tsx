430247e238301480bb008f99dbb851d17737abda
Zuong <quocvuongle.ct@gmail.com>
2023-12-23 21:26:54 +0700

    update home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import ImageItem from "@/components/ImageItem";
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="flex justify-center">
+            <div className="flex flex-wrap gap-3">
+                <div>
+                    <ImageItem src="./test1.jpg"></ImageItem>
+                </div>
+                <div>
+                    <ImageItem src="./test2.jpg"></ImageItem>
+                </div>
+                <div>
+                    <ImageItem src="./test1.jpg"></ImageItem>
+                </div>
+                <div>
+                    <ImageItem src="./test3.jpg"></ImageItem>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
